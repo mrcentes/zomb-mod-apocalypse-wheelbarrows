1439a263e8e394f4fbdb46e0a1301239dd9c1dd5
Patrick Culligan <pculligan@patrickculligan.com>
2026-08-08 17:17:03 -0500

    first commit

---
+++ mods/PFG_ONLY/42/media/lua/client/PFG_Wheelbarrow/TimedAction/WheelbarrowB42limit.lua
@@ -0,0 +1,33 @@
+if PFGMenu then return end
+local b41_capacity = (PFGMenu and PFGMenu.capacityOverride) or 180;
+local original_ISInventoryTransferAction_isValid = ISInventoryTransferAction.isValid
+function ISInventoryTransferAction:isValid()
+    
+    local val = original_ISInventoryTransferAction_isValid(self)
+    --b42 30 50 capacity bypass
+    local bypassB42Limit = false;
+    if self.destContainer ~= nil and self.destContainer.getType and PFGMenu.typesTable[self.destContainer:getType()] then
+        bypassB42Limit = (b41_capacity > (self.destContainer:getContentsWeight() + self.item:getWeight()))
+        return bypassB42Limit
+    end
+    --end b42
+    return val
+end
+
+local original_ISInventoryPane_canPutIn = ISInventoryPane.canPutIn
+function ISInventoryPane:canPutIn()
+    local bypassB42Limit = false;
+    if self.inventory ~= nil and self.inventory.getType and PFGMenu.typesTable[self.inventory:getType()] then
+        local items = {}
+        items=ISInventoryPane.getActualItems(ISMouseDrag.dragging)
+        bypassB42Limit = (b41_capacity > (self.inventory:getContentsWeight() + items[1]:getWeight()))
+        -- if bypassB42Limit then
+        --     local playerObj = getSpecificPlayer(self.player)
+        -- 	ISTimedActionQueue.add(ISInventoryTransferAction:new(playerObj, items[1], items[1]:getContainer(), self.inventory))
+        -- end
+        return bypassB42Limit
+    end
+
+    local val = original_ISInventoryPane_canPutIn(self)
+    return val
+end
---
+++ mods/Wheelbarrow_from_Hydrocraft/media/lua/client/TimedAction/PFGPushAction.lua
@@ -0,0 +1,76 @@
+require "TimedActions/ISBaseTimedAction"
+
+PFGPushAction = ISBaseTimedAction:derive("PFGPushAction")
+
+function PFGPushAction.isValid(args)
+	
+	for i,v in ipairs(PFGMenu.typesTable) do
+		if args["character"]:getInventory():contains(tostring(v)) then
+			return false
+		end
+	end
+	return true
+end
+
+function PFGPushAction:waitToStart()
+	return false
+end
+
+function PFGPushAction:update()
+
+end
+
+function PFGPushAction:start()
+	
+end
+
+function PFGPushAction:stop()
+	ISBaseTimedAction.stop(self);
+end
+
+function PFGPushAction:perform()
+	
+	local inventoryItem = self.item:getItem()
+	if self.item:getSquare() then 
+		self.item:getSquare():transmitRemoveItemFromSquare(self.item)
+	end
+	self.item:removeFromWorld()
+	self.item:removeFromSquare()
+	self.item:setSquare(nil)
+	inventoryItem:setWorldItem(nil)
+	inventoryItem:setJobDelta(0.0)
+	self.inventory:setDrawDirty(true)
+	self.inventory:AddItem(inventoryItem)
+	PFGPushAction.equipWeapon(inventoryItem,false,true,self.character:getPlayerNum())
+
+	ISBaseTimedAction.perform(self)
+end
+
+-- Function that equip the selected weapon
+PFGPushAction.equipWeapon = function(weapon, primary, twoHands, player)
+	local playerObj = getSpecificPlayer(player)
+	-- Drop corpse or generator
+	if isForceDropHeavyItem(playerObj:getPrimaryHandItem()) then
+		ISTimedActionQueue.add(ISUnequipAction:new(playerObj, playerObj:getPrimaryHandItem(), 50));
+	end
+	-- if weapon isn't in main inventory, put it there first.
+	-- FIX BUG cant retrive when store in vehicle truck
+	--ISInventoryPaneContextMenu.transferIfNeeded(playerObj, weapon)
+    -- Then equip it.
+	ISTimedActionQueue.add(ISEquipWeaponAction:new(playerObj, weapon, 50, primary, twoHands));
+end
+
+function PFGPushAction:new( item, player)
+
+	local o = {}
+	setmetatable( o, self)
+	self.__index = self
+	o.maxTime = 50
+	o.character = player
+	o.inventory = player:getInventory()
+	o.item = item
+	o.stopOnWalk = false
+	o.stopOnRun = false
+	
+	return o
+end
